b3593e49e543e7319deec688100ea66fac4b4d70
fgabrieli@gmail.com <fgabrieli@gmail.com>
2014-11-19 19:16:29 -0300

    Adding jQuery ui autocomplete.

--- www/js/modules/recipe/Recipe.js
+++ www/js/modules/recipe/Recipe.js
@@ -1,46 +1,55 @@
 que.Recipe = {
   ngController : function($scope, $element) {
+    var t = que.Recipe;
+
     var DEFAULT_INGREDIENT = {
       name : ''
     };
-
-    // XXX: delete me when backend is ready
-    $scope.suggestedIngredients = [ {
-      name : 'sal'
-    }, {
-      name : 'lechuga'
-    }, {
-      name : 'zanahoria'
-    } ];
-
-    $scope.hasSuggestedIng = false;
-
-    setupIngredients();
+    
+    var INPUT_CLASS = 'ingredient-input';
 
     function setupIngredients() {
       $scope.ingredients = [];
       newIngredient();
+      
+      setTimeout(function() {
+        $scope.$apply();
+        
+        $('.' + INPUT_CLASS).autocomplete({
+          source : [ 'test', 'test1', 'test2' ]
+        });
+      }, 0);
+    }
+
+    $(window).load(function() {
+      setupIngredients();
+    });
+
+    // Last ingredient input id
+    
+    var lastInputId = 0;
+
+    function getNewInputId() {
+      return ++lastInputId;
     }
 
     function newIngredient() {
-      $scope.ingredients.push($.extend({}, DEFAULT_INGREDIENT));
+      var newId = getNewInputId();
+      $scope.ingredients.push($.extend({
+        id : newId
+      }, DEFAULT_INGREDIENT));
 
       setTimeout(function() {
         $scope.$apply();
+        
+        $('#ingredient-' + newId).autocomplete({
+          source : [ 'test', 'test1', 'test2' ]
+        });
       }, 0);
     }
 
     $scope.newIngredient = function() {
       newIngredient();
     };
-    
-    $scope.suggestIngredients = function(ingredient) {
-      console.log(ingredient.name);
-      $scope.hasSuggestedIng = true;
-      
-      // Set the position for the box
-
-      console.log(this);
-    }
   }
 }
